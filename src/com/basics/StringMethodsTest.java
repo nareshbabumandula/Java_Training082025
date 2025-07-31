c38e67cdfcff1d49f788bc394ef9e2679b65a82b
[nareshbabumandula] <naresh223@gmail.com>
2025-07-31 09:59:43 +0530

    31-Jul-2025: Naresh: String methods in Java

---
+++ src/com/basics/StringMethodsTest.java
@@ -0,0 +1,16 @@
+package com.basics;
+
+public class StringMethodsTest {
+
+	public static void main(String[] args) {
+		String str = "arif";
+		String s = "MANGULAL";
+		String s1 = "hello world welcome to java se training";
+		System.out.println(str.toUpperCase());
+		System.out.println(s.toLowerCase());
+		System.out.println(s.length());
+		System.out.println(s1.contains("java"));
+
+	}
+
+}
