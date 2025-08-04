7984d07b06d04072efe3aa8d2499da3980b3fe08
lenovo <lenovo@DESKTOP-UF3NHG6>
2025-08-04 18:23:38 +0530

    04-Aug-25: Niklesh string methods and for loops example

--- src/com/basics/StringMethodsTest.java
+++ src/com/basics/StringMethodsTest.java
@@ -6,9 +6,12 @@ public class StringMethodsTest {
 		String str = "arif";
 		String s = "MANGULAL";
 		String s1 = "hello world welcome to java se training";
+		String s2 = "";
+		String s3 = null;
+		String s4 = "      Selenium automation               ";
 		System.out.println(str.toUpperCase());
 		System.out.println(s.toLowerCase());
-		System.out.println(s.length());
+		System.out.println("Length of the string is:"+ s.length());
 		System.out.println(s1.contains("java"));
 		System.out.println(s1.charAt(0));
 		System.out.println(s1.charAt(1));
@@ -16,9 +19,40 @@ public class StringMethodsTest {
 		System.out.println(s1.concat(" program"));
 		System.out.println("hello");
 		System.out.println(str.equals("arif"));
-		System.out.println(s1.concat(" platform"));
-		System.out.println(str.equals("arif"));
+		System.out.println(s1.concat(" program"));
+		System.out.println(str.equals("ARIF"));
+		System.out.println(str.equalsIgnoreCase("ARIF"));
+		System.out.println(s1.isEmpty());
+		System.out.println(s2.isEmpty());
+		//System.out.println(s3.isEmpty());
+		//System.out.println(s3.length());
+		System.out.println(s1.substring(2, 10));
+		System.out.println(s4);
+		System.out.println(s4.trim());
+
+		char[] res =s1.toCharArray();
+
+		System.out.println(res[0]);
+		System.out.println(res[1]);
+
+		for(char c : res) {
+			System.out.println(c + " ");
+		}
+		for (int i = 0; i < res.length; i++) {
+			System.out.println("chracter found is: " + res[i]);
+		}
+		
+			String sText = "MANGULAL";
+			String rev="";
+			
+		for (int i = sText.length()-1; i>=0; i--)	 {
+			rev+=sText.charAt(i);
+		}
+		System.out.println("Reverse of " + sText + " is: " + rev );
+		
 	}
 	
+		
+	}
 
-}
+
