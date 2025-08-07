865f3bc1a9a867970b83185667c6aeacc09b2815
arifh1804 <arifh@192.168.1.12>
2025-08-07 16:13:44 +0530

    07-Aug-2025: Inheritance and Polymorphism in Java OOPS

--- src/com/oops/B.java
+++ src/com/oops/B.java
@@ -1,23 +1,22 @@
 package com.oops;
 
 public class B extends A {
-
-	public B() {
-		System.out.println("Executing B class constructor..!");
-	}
 	
-	void subtraction(){
-		int c=a-b;
-		System.out.println("Subtraction of a and b is: " +c);
-	}
-	
+    public B() {
+    	System.out.println("Executing B class constructor..!");
+    }
+    
+    void substraction() {
+    	int c=a-b;
+    	System.out.println("Substraction of a and b is: " +c);
+    }
 	public static void main(String[] args) {
-	  B b = new B();
-	  b.subtraction();
-	  b.addition();
-	  System.out.println(b.a);
-	  System.out.println(b.b);
-	  
+		B b = new B();
+		b.substraction();
+		b.addition();
+		System.out.println(b.a);
+		System.out.println(b.b);
+		
 	}
 
 }
