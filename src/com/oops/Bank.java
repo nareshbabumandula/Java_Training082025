f2553367d247254be0612a419f2a50b4aeb431c5
Mandula Naresh Babu <naresh223@gmail.com>
2025-08-13 12:47:27 +0530

    13-Aug-2025: Naresh: Interface in Java (#19)
    * 01-Aug:2025: Naresh: Added replaceAll method exmaple
    
    * Added equals from the String class (#2)
    
    Co-authored-by: ajayp <ajayp@Bapi>
    
    * 04-Aug-2025: Naresh: String methods and for loop example
    
    * Added equals from the String class (#2)
    
    Co-authored-by: ajayp <ajayp@Bapi>
    
    * 04-Aug-2025: Naresh: String methods and for loop example
    
    * 06-Aug-2025: Naresh: Class vs Object and Creating ObjRef for a class
    
    * 07-Aug-2025: Inheritance and Polymorphism in Java OOPS
    
    * Added endswith method example from String class
    
    * 11-Aug-2025: Naresh: Added endswith method from String class
    
    * Created Theatre abstract class
    
    * Abstraction in Java
    
    * Abstraction in Java
    
    * Test
    
    * Test Commit
    
    * Abstraction in Java
    
    * Encapsulation in Java using Banking as an example
    
    * 13-Aug-2025: Naresh: Interface in Java
    
    * 13-Aug-2025: Naresh: Interface in Java
    
    * 13-Aug-2025: Naresh: Interface in Java
    
    * 13-Aug-2025: Naresh: Interface in Java
    
    ---------
    
    Co-authored-by: aditi835 <adithipramanik@gmail.com>
    Co-authored-by: ajayp <ajayp@Bapi>

---
+++ src/com/oops/Bank.java
@@ -0,0 +1,15 @@
+package com.oops;
+
+public interface Bank {
+	
+	enum AccountType  {SAVINGS, CURRENT, FIXED_DEPOSIT}
+	
+	// abstract methods
+    void openAccount(String customerId, AccountType type);
+    void deposit(String customerId, double amount);
+    void withdraw(String customerId, double amount);
+    double getBalance(String customerId);
+    double getInterestRate(AccountType type);
+    
+       
+}
