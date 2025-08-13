53b7e4e43180049399150abf4573e83768f4260c
[nareshbabumandula] <naresh223@gmail.com>
2025-08-13 12:44:43 +0530

    13-Aug-2025: Naresh: Interface in Java

---
+++ src/com/oops/HDFC.java
@@ -0,0 +1,40 @@
+package com.oops;
+
+public class HDFC implements RBI{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void openAccount(String customerId, AccountType type) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deposit(String customerId, double amount) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void withdraw(String customerId, double amount) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double getBalance(String customerId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getInterestRate(AccountType type) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
