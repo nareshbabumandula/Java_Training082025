bef2ceb468482959edfa67a88f00d514f046d9f3
Mandula Naresh Babu <naresh223@gmail.com>
2025-08-19 10:42:06 +0530

    19-Aug-2025: Naresh: Interface in Java and Selenium WebDriver Architecture (#22) (#23)
    * 01-Aug:2025: Naresh: Added replaceAll method exmaple
    
    * Added equals from the String class (#2)
    
    
    
    * 04-Aug-2025: Naresh: String methods and for loop example
    
    * Added equals from the String class (#2)
    
    
    
    * 04-Aug-2025: Naresh: String methods and for loop example
    
    * 06-Aug-2025: Naresh: Class vs Object and Creating ObjRef for a class
    
    * 07-Aug-2025: Inheritance and Polymorphism in Java OOPS
    
    * Added endswith method example from String class
    
    * 11-Aug-2025: Naresh: Added endswith method from String class
    
    * Created Theatre abstract class
    
    * Abstraction in Java
    
    * Abstraction in Java
    
    * Test
    
    * Test Commit
    
    * Abstraction in Java
    
    * Encapsulation in Java using Banking as an example
    
    * 13-Aug-2025: Naresh: Interface in Java
    
    * 13-Aug-2025: Naresh: Interface in Java
    
    * 13-Aug-2025: Naresh: Interface in Java
    
    * 13-Aug-2025: Naresh: Interface in Java
    
    * 19-Aug-2025: Naresh: Interface in Java and Selenium WebDriver
    Architecture
    
    ---------
    
    Co-authored-by: aditi835 <adithipramanik@gmail.com>
    Co-authored-by: ajayp <ajayp@Bapi>

--- src/com/selenium/scripts/BrowserTest.java
+++ src/com/selenium/scripts/BrowserTest.java
@@ -1,12 +1,22 @@
 package com.selenium.scripts;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BrowserTest {
 	
-	
+	static WebDriver driver;
 
 	public static void main(String[] args) {
-		
-
-	}
-
+		driver = new ChromeDriver();
+		driver.get("https://www.mycontactform.com");
+		driver.manage().window().maximize();
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getTitle());
+		driver.findElement(By.id("user")).sendKeys("aditi");
+		driver.findElement(By.id("pass")).sendKeys("Secure*123");
+		driver.quit();
+  }
+ 
 }
